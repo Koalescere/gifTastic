f4b2a86ac6dfca1689d6b0e08034409eaf2f86c1
Test123 <Test123@gavins-mbp.home>
2019-08-28 12:09:19 -0400

    <giphy reached, image pull issue>

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,15 +9,12 @@
         for (var i=0; i < motoMachines.length; i++) {
           var a = $("<button>");
           a.addClass("moto");
-          a.attr("data-name", motoMachines[i]);
+          a.attr("data-type", motoMachines[i]);
           a.text(motoMachines[i]);
           $("#moto-container").append(a);
-          getImage();
         }
     }
-    //createButtons(); check that buttons are made
-
-
+    //form field entry converted into a buttom
     $("#add-moto").on("click", function(event){
         event.preventDefault();
         var moto = $("#newmoto").val().trim();
@@ -27,30 +24,31 @@
 
     createButtons();
 
-    function getImage(){
-        $("button").on("click", function() {
-            var a = $this.attr("data-type", motoMachines[i]);
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + a +"&api_key=QZ2LOfJhGjcBK66FkyDBULoGD4CFu8Wp&limit=5%22";
-    
-
-            $.ajax({
-                url:  queryURL,
-                method:  "GET"
-            })
-            .then(function(response){
-                console.log(queryURL);
-                console.log(response);
-            })     
-            var results= response.data;
-            for (var i=0; i<results.length;i++) {
-                var motoDiv = $("<div>");
-                var p = $("<p>").text("Rating: " + results[i].rating);
-                var motoImage = $("<img>");
-                motoDiv.attr("src", results[i].imgages.fixed_height.url);
-                motoDiv.append(p);
-                motoDiv.append(motoImage);
-                $("#gifs-appear-here").prepend(motoDiv);
-            }
+    //listening event to call giphy
+    $("button.moto").on("click", function() {
+        var type = $(this).data("type");
+        console.log(type);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=QZ2LOfJhGjcBK66FkyDBULoGD4CFu8Wp&limit=5%22";
+        $.ajax({
+            url:  queryURL,
+            method:  "GET"
         })
-    }       
+        .then(function(response){
+            console.log(queryURL);
+            //console.log(response);
+       
+        var results= response.data;
+        
+        for (var i=0; i<results.length;i++) {
+            var motoDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var motoImage = $("<img>");
+            motoImage.attr("src", results[i].imgages.fixed_height_still.url);
+            motoDiv.append(p);
+            motoDiv.append(motoImage);
+            $("#gifs-appear-here").prepend(motoDiv);
+        }    
+    });
+    });
+           
     
